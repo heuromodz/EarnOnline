2fdf9ff47ec17daf411c795778c2d97c6a4fce3a
heuromodz <101259176+heuromodz@users.noreply.github.com>
2022-03-10 13:10:15 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,33 +6,5 @@ switchers.forEach(item => {
 		this.parentElement.classList.add('is-active')
 	})
 })
-      
-        var form = document.getElementById('sheetdb-form');
-        form.addEventListener("submit", e => {
-          e.preventDefault();
-          fetch(form.action, {
-              method : "POST",
-              body: new FormData(document.getElementById("sheetdb-form")),
-          }).then(
-              response => response.json()
-          ).then((html) => {
-            // you can put any JS code here
-            window.open('success.html', '_blank');
 
-          });
-          
-        });
-            var form = document.getElementById('sheetdb');
-        form.addEventListener("submit", e => {
-          e.preventDefault();
-          fetch(form.action, {
-              method : "POST",
-              body: new FormData(document.getElementById("sheetdb")),
-          }).then(
-              response => response.json()
-          ).then((html) => {
-            // you can put any JS code here
-            window.open('success.html', '_blank');
 
-          });
-        });
